b4d6714b30f7632fe740eddd9d7984f4016b0e27
aquino35 <osvaldo.aquino@upr.edu>
2022-01-07 12:49:42 -0400

    Student documentation (python)

--- src/python/lib/Student.py
+++ src/python/lib/Student.py
@@ -6,18 +6,79 @@ from typing import ClassVar
 @dataclass
 class Student(Person):
     """
-    TODO: Document
+    ## Help on the Student module:
+    ---------------------
+    ### Name
 
-    @property decorator -> getters for class 
-    @setter decorator -> setters for class
+        Student 
+
+    ---------------------
+    
+    ### Description 
+
+        The student class is developed to simulate a person that is a student. 
+
+        This class inherits attributes and methods from the Person class.
+
+        @param first_name - first name of student. 
+
+        @param last_name - last name of student.
+
+        @param gender - gender of student.
+
+        @param height - height of student. 
+
+        @param weight - weight of student.
+
+        @param age - age of student. 
+
+        @param institution - institution of the student.
+
+        @param major - major of the student.
+
+        @property decorator: getters for class. 
+
+        @setter decorator: setters for class.
+
+        @example: student = Student("Osvaldo", "Aquino", "M", "5'8", 170, 25, "UPRM", "ICOM")
+
+    ---------------------
+
+    ### Package Contents
+
+        - _first_name
+        - _last_name
+        - _gender
+        - _height
+        - _age
+        - _institution
+        - _major
+        - _person_count 
+        - _student_count
+        - talk()
+        - calculate()
+        - first_name() || first_name(first_name:str)
+        - last_name() || last_name(last_name:str)
+        - gender() || gender(gender:str)
+        - weight() || weight(weight:int)
+        - age() || age(age:int)
+        - institution() || institution(institution:str)
+        - major() || major(major:str)
+
+    ---------------------
+
+    @author: Osvaldo Aquino
     """
 
-    _institution: str
-    _major : str
-    _student_count : ClassVar[int] = 0
+    _institution: str # represents institution of the student
+    _major : str # represents major of the student
+    _student_count : ClassVar[int] = 0 # static variable used to keep track of each student that is created.
 
 
     def __post_init__(self):
+
+        """ Post init function used to increment the global class variables used in this object."""
+        
         Student._person_count+=1
         Student._student_count+=1
 
@@ -25,29 +86,57 @@ class Student(Person):
 
     @property
     def institution(self) -> str:
+
+        """ @return: Institution of the student.
+            @example: student.institution """
+
         return self._institution
 
 
     @property
     def major(self) -> str:
+
+        """ @return: Major of the student.
+            @example: student.major """
+
         return self._major
 
 
     @institution.setter
     def institution(self, institution: str) -> None:
+
+        """ Sets the institution of the student given a string.
+            @param institution: String that represents the new institution that will be set.
+            @example: student.institution = "UPRM" """
+
         self._institution = institution
 
 
     @major.setter
     def major(self, major: str) -> None:
+
+        """ Sets the major of the student given a string.
+            @param major: String that represents the new major that will be set.
+            @example: student.major = "ICOM" """
+
         self._major = major
 
 
     def talk(self) -> str:
+
+        """ Prints out useful information about the student object.
+            @example: student.talk()"""
+
         print(f'Hello! My name is {self._first_name} {self._last_name}, I am a student at {self.institution}, studying {self._major}')
 
     
-    def calculate(self, *args):
+    def calculate(self, *args: int):
+
+        """ Calculates the final grade of a student given a set of grades.
+            @param *args: Integers representing all the grades the student has accumulated.
+            @print: The final grade of the student
+            @example: student.calculate(100,100,80,100)
+            @example: student.calculate(90,85) """
 
         num_grades = len(args)
         final_grade = sum(args)/num_grades
--- src/python/lib/Worker.py
+++ src/python/lib/Worker.py
@@ -6,42 +6,111 @@ from typing import ClassVar
 @dataclass
 class Worker(Person):
     """
-    TODO: Document
+    ## Help on the Worker module:
+    ---------------------
+    ### Name
 
-    @property decorator -> getters for class 
-    @setter decorator -> setters for class
+        Worker 
+
+    ---------------------
+
+    ### Description 
+
+        The worker class is developed to simulate a person that is a worker. 
+
+        This class inherits attributes and methods from the Person class.
+
+        @param first_name - first name of worker. 
+
+        @param last_name - last name of worker.
+
+        @param gender - gender of worker.
+
+        @param height - height of worker. 
+
+        @param weight - weight of worker.
+
+        @param age - age of worker. 
+
+        @param weekly hours - weekly hours of worker.
+
+        @param salary - salary of worker.
+
+        @property decorator: getters for class. 
+
+        @setter decorator: setters for class.
+
+        @example: worker = Worker("Osvaldo", "Aquino", "M", "5'8", 170, 22, 40, 50000)
+
+    ---------------------
+
+    ### Package Contents
+
+        - _first_name
+        - _last_name
+        - _gender
+        - _height
+        - _age
+        - _weekly_hours
+        - _salary
+        - _person_count 
+        - _worker_count
+        - talk()
+        - first_name() || first_name(first_name:str)
+        - last_name() || last_name(last_name:str)
+        - gender() || gender(gender:str)
+        - weight() || weight(weight:int)
+        - age() || age(age:int)
+        - weekly_hours() || weekly_hours(weekly_hours:int)
+        - salary() || salary(salary:int)
+
+    ---------------------
+
+    @author: Osvaldo Aquino
     """
 
-    _weekly_hours: int
-    _salary : int
-    _worker_count : ClassVar[int] = 0
+    _weekly_hours: int # represents weekly hours of the worker
+    _salary : int # represents the salary of the worker
+    _worker_count : ClassVar[int] = 0 # static variable used to keep track of each worker that is created.
 
 
     def __post_init__(self):
-        Worker._person_count+=1
+        """ Post init function used to increment the global class variables used in this object."""
+        Worker._person_count+=1 
         Worker._worker_count+=1
-
 
 
     @property
     def weekly_hours(self) -> int:
+        """ @return: Weekly hours of the worker.
+            @example: worker.weekly_hours """
         return self._weekly_hours
 
 
     @property
     def salary(self) -> int:
+        """ @return: Salary of the worker.
+            @example: worker.salary """
         return self._salary
 
 
     @weekly_hours.setter
     def weekly_hours(self, weekly_hours: int) -> None:
+        """ Sets the weekly hours of the worker given an integer.
+            @param weekly_hours: Integer that represents the new weekly hours that will be set.
+            @example: worker.weekly_hours = 40 """
         self._weekly_hours = weekly_hours
 
 
     @salary.setter
     def salary(self, salary: int) -> None:
+        """ Sets the salary of the worker given an integer.
+            @param salary: Integer that represents the new salary that will be set.
+            @example: worker.salary = 90000 """
         self._salary = salary
 
 
     def talk(self) -> str:
+        """ This method is used to print out useful information about the worker object.
+            @example: worker.talk()"""
         print(f'Hello! My name is {self._first_name} {self._last_name}, I am a worker that works {self.weekly_hours} a week and I have a salary of {self._salary}.')
